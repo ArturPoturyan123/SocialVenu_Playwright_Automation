4f89e64081147fbe00976248f1aa3a8fdb152f79
ArturPoturyan123 <poturyan@gmail.com>
2024-10-09 22:54:54 +0400

    fix

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,11 +1,15 @@
-import { test } from '@playwright/test'
-import LoginPage from '../pages/LoginPage';
-// const LoginPage = require('../pages/login');
+import { expect,test } from "@playwright/test";
+import LoginPage from "../pages/LoginPage";
+import logger from "../utils/LoggerUtil";
 
 
-test('Login test', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    
-    await loginPage.quickLogin('arthurp@doublecoconut.com', '123456');
+test("Login test", async ({ page }) => {
+  const loginPage = new LoginPage(page);
 
+  // await loginPage.quickLogin('arthurp@doublecoconut.com', '123456');
+  await loginPage.navigateToLoginPage();
+  await loginPage.fillUsername("arthurp@doublecoconut.com");
+  await loginPage.fillPassword("123456");
+  const homePage = await loginPage.clickLoginButton();
+  logger.info("Test for login is completed");
 });
